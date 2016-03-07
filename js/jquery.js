c09d24c2d965111049bf8d3ab4017ede06eaeb96
quebell7 <kbell.marinescu@gmail.com>
2016-03-07 15:16:47 -0800

    javascript updates - 3.7

---
+++ js/jquery.js
@@ -0,0 +1,77 @@
+// $(function pageReady() {
+// 	$(document).ready(function() {
+//    } );
+
+
+
+
+$(function moreContent (){
+	$(".blog-link").click(function moreContent() {
+       $(".panel").slideToggle("slow")
+   } );
+} );
+
+$(function hideLink (){
+	$(".blog-link").click(function hideLink() {
+       $(this).hide()
+   } );
+} );
+
+$(function showLink (){
+	$(".blog-link-less").click(function showLink() {
+       $(this).hide()
+   } );
+} );
+
+// $(function showLink (){
+// 	$(".blog-link-less").click(function showLink() {
+//        $(this).show()
+//    } );
+// } );
+
+
+
+// $(document).ready(function(){
+//     $(".blog-link").click(moreContent(){
+//         $("#panel").slideToggle("slow");
+//     });
+// });
+
+
+
+
+// $(function lessContent (){
+// 	$(".blog-link").click(function lessContent() {
+//        $(".post p").hide("500")
+//    } );
+// } );
+
+
+
+
+//   $(".post").on("click", "p", function() {
+//     $(this).find(".more-content").slideToggle();
+//     $(this).find(".ion-chevron-down").toggleClass("ion-chevron-up");
+//   });
+// });
+
+// $(document).ready(function(){
+//         $('.blogLinkClosed').hide();
+//     });
+
+
+// $(document).ready(function(){
+//     $(".blogLinkOpen").click(function(){
+//         $("p").show("blog-post p");
+//     });
+//     $(".blogLinkClosed").click(function(){
+//         $("p").hide("blog-post p");
+//     });
+// });
+// $(".blog-link").click(showMoreText);
+
+
+        // All of you site's JavaScript goes here       
+
+
+
